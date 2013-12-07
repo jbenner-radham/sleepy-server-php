076435d5a883f1ece35fbf3bc79ab5883f079ffe
James Benner <james.benner@gmail.com>
2013-12-07 00:26:35 -0500

    Update php_sleepy.h

--- php_sleepy.h
+++ php_sleepy.h
@@ -1,7 +1,11 @@
 #define PHP_SLEEPY_EXTNAME "sleepy"
-#define PHP_SLEEPY_VERSION "0.0.1"
+//#define PHP_SLEEPY_VERSION "0.0.1"
+// http://www.php.net/manual/en/internals2.structure.modstruct.php#internals2.structure.modstruct.filling-it-in.counter-mod-ex
+#define PHP_SLEEPY_VERSION NO_VERSION_YET
 
 PHP_MINIT_FUNCTION(sleepy);
+PHP_RINIT_FUNCTION(sleepy);
+
 PHP_MSHUTDOWN_FUNCTION(sleepy);
 
 PHP_FUNCTION(sleepy);
