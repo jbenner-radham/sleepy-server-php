bcc69c87593b03a37f65191d6719427c2fd571b9
James Benner <james.benner@gmail.com>
2013-12-07 19:26:15 -0500

    Updated sleepy.c to use class constants.

--- sleepy.c
+++ sleepy.c
@@ -28,26 +28,33 @@ PHP_MINIT_FUNCTION(sleepy)
     ce.create_object = NULL;
     ce_Sleepy = zend_register_internal_class(&ce TSRMLS_CC);
 
+    /* Declare your class constants after you register the class, duh me... */
+    //zend_declare_class_constant_bool(ce_Sleepy, ZEND_STRL("SLEEPY_URI"), 0 TSRMLS_DC);
+
     return SUCCESS;
 }
 
 PHP_RINIT_FUNCTION(sleepy)
 {
-    /*
-     * -----------
-     * Access via:
-     * -----------
-     * echo Sleepy::$request_method;
-     * echo '<br>';
-     * echo Sleepy::$request_uri;
-     */
-
     if (SG(request_info).request_method != NULL) {
-        zend_declare_property_string(ce_Sleepy, ZEND_STRL("request_method"), SG(request_info).request_method, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC TSRMLS_CC);
+        //zend_declare_property_string(ce_Sleepy, ZEND_STRL("request_method"), SG(request_info).request_method, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC TSRMLS_CC);
+        zend_declare_class_constant_string(ce_Sleepy, ZEND_STRL("REQUEST_METHOD"), SG(request_info).request_method TSRMLS_DC);
+    } else {
+        zend_declare_class_constant_bool(ce_Sleepy, ZEND_STRL("REQUEST_METHOD"), 0 TSRMLS_DC);
     }
     
-    if (SG(request_info).request_uri != NULL)
-        zend_declare_property_string(ce_Sleepy, ZEND_STRL("request_uri"), SG(request_info).request_uri, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC TSRMLS_CC);
+    if (SG(request_info).request_uri != NULL) {
+        //zend_declare_property_string(ce_Sleepy, ZEND_STRL("request_uri"), SG(request_info).request_uri, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC TSRMLS_CC);
+        zend_declare_class_constant_string(ce_Sleepy, ZEND_STRL("REQUEST_URI"), SG(request_info).request_uri TSRMLS_DC);
+    } else {
+        zend_declare_class_constant_bool(ce_Sleepy, ZEND_STRL("REQUEST_URI"), 0 TSRMLS_DC);
+    }
+
+    if (SG(request_info).query_string != NULL) {
+        zend_declare_class_constant_string(ce_Sleepy, ZEND_STRL("QUERY_STRING"), SG(request_info).query_string TSRMLS_DC);
+    } else {
+        zend_declare_class_constant_bool(ce_Sleepy, ZEND_STRL("QUERY_STRING"), 0 TSRMLS_DC);
+    }
 
     return SUCCESS;
 }
