78c6ecc51e5e5e50e84ab18a60d16a741b4b9bf6
James Benner <james.benner@gmail.com>
2013-12-07 23:23:19 -0500

    Cleaned up a little and added a RESPONSE_CODE constant.

--- sleepy.c
+++ sleepy.c
@@ -36,24 +36,32 @@ PHP_MINIT_FUNCTION(sleepy)
 
 PHP_RINIT_FUNCTION(sleepy)
 {
-    if (SG(request_info).request_method != NULL) {
-        //zend_declare_property_string(ce_Sleepy, ZEND_STRL("request_method"), SG(request_info).request_method, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC TSRMLS_CC);
+    /* Request Method */
+    if (SG(request_info).request_method) {
         zend_declare_class_constant_string(ce_Sleepy, ZEND_STRL("REQUEST_METHOD"), SG(request_info).request_method TSRMLS_DC);
     } else {
         zend_declare_class_constant_bool(ce_Sleepy, ZEND_STRL("REQUEST_METHOD"), 0 TSRMLS_DC);
     }
     
-    if (SG(request_info).request_uri != NULL) {
-        //zend_declare_property_string(ce_Sleepy, ZEND_STRL("request_uri"), SG(request_info).request_uri, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC TSRMLS_CC);
+    /* Request URI */
+    if (SG(request_info).request_uri) {
         zend_declare_class_constant_string(ce_Sleepy, ZEND_STRL("REQUEST_URI"), SG(request_info).request_uri TSRMLS_DC);
     } else {
         zend_declare_class_constant_bool(ce_Sleepy, ZEND_STRL("REQUEST_URI"), 0 TSRMLS_DC);
     }
 
-    if (SG(request_info).query_string != NULL) {
+    /* Query String */
+    if (SG(request_info).query_string) {
         zend_declare_class_constant_string(ce_Sleepy, ZEND_STRL("QUERY_STRING"), SG(request_info).query_string TSRMLS_DC);
     } else {
         zend_declare_class_constant_bool(ce_Sleepy, ZEND_STRL("QUERY_STRING"), 0 TSRMLS_DC);
+    }
+
+    /* Response Code */
+    if (SG(sapi_headers).http_response_code) {
+        zend_declare_class_constant_long(ce_Sleepy, ZEND_STRL("RESPONSE_CODE"), SG(sapi_headers).http_response_code TSRMLS_DC);
+    } else {
+        zend_declare_class_constant_bool(ce_Sleepy, ZEND_STRL("RESPONSE_CODE"), 0 TSRMLS_DC);
     }
 
     return SUCCESS;
