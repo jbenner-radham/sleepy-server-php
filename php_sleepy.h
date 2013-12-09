8b9608e317b405f588a06244f3ae802fbf9a52bd
James Benner <james.benner@gmail.com>
2013-12-09 01:05:51 -0500

    Update php_sleepy.h

--- php_sleepy.h
+++ php_sleepy.h
@@ -3,9 +3,12 @@
 // http://www.php.net/manual/en/internals2.structure.modstruct.php#internals2.structure.modstruct.filling-it-in.counter-mod-ex
 #define PHP_SLEEPY_VERSION NO_VERSION_YET
 
+//extern zend_module_entry sleepy_module_entry;
+
 PHP_MINIT_FUNCTION(sleepy);
 PHP_RINIT_FUNCTION(sleepy);
 
 //PHP_MSHUTDOWN_FUNCTION(sleepy);
 
 //PHP_FUNCTION(sleepy);
+PHP_METHOD(Sleepy, test);
