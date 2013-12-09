d71cd95690717b07fbcbdfc800cfe77897ff2003
James Benner <james.benner@gmail.com>
2013-12-09 01:05:11 -0500

    Minor update, it references the "test" method too.

--- sleepy.c
+++ sleepy.c
@@ -2,17 +2,40 @@
 #include "php_sleepy.h"
 #include <SAPI.h>
 #include <zend_API.h>
+#include "test.c"
+
+/*
+ *  #define ZEND_STRL(str)      (str), (sizeof(str)-1)
+ *  #define ZEND_STRS(str)      (str), (sizeof(str))
+ *
+ *  Cast a value to "zend_bool" which is a typedef for "unsigned char".
+ *  #define ZEND_TRUTH(x)       ((x) ? 1 : 0)
+ *
+ *  [Zend/zend.h]
+ *  #define SUCCESS 0
+ *  #define FAILURE -1  
+ * 
+ *  [zend_API.h]
+ *  #define ZEND_FE_END            { NULL, NULL, NULL, 0, 0 }
+ *  #define ZEND_NS_NAME(ns, name)          ns "\\" name
+ *  #define ZEND_ME(classname, name, arg_info, flags)   ZEND_FENTRY(name, ZEND_MN(classname##_##name), arg_info, flags)
+ */
+
+const zend_function_entry sleepy_class_methods[] = {
+    PHP_ME(Sleepy, test, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
+    {NULL, NULL, NULL}
+};
 
 zend_module_entry sleepy_module_entry = {
     STANDARD_MODULE_HEADER,
     PHP_SLEEPY_EXTNAME,
-    NULL,               /* Extension Function Entries         */
-    PHP_MINIT(sleepy),  /* MINIT     - Module Initialization  */
-    NULL,               /* MSHUTDOWN - Module Shutdown        */
-    PHP_RINIT(sleepy),  /* RINIT     - Request Initialization */
-    NULL,               /* RSHUTDOWN - Request Shutdown       */
-    NULL,               /* MINFO     - Module Information     */
-    PHP_SLEEPY_VERSION, /* Extension Version Number           */
+    sleepy_class_methods,      /* Extension Function Entries         */
+    PHP_MINIT(sleepy),         /* MINIT     - Module Initialization  */
+    NULL,                      /* MSHUTDOWN - Module Shutdown        */
+    PHP_RINIT(sleepy),         /* RINIT     - Request Initialization */
+    NULL,                      /* RSHUTDOWN - Request Shutdown       */
+    NULL,                      /* MINFO     - Module Information     */
+    NO_VERSION_YET,            /* Extension Version Number           */
     STANDARD_MODULE_PROPERTIES
 };
 
@@ -24,7 +47,7 @@ PHP_MINIT_FUNCTION(sleepy)
 {
     zend_class_entry ce;
 
-    INIT_CLASS_ENTRY(ce, "Sleepy", NULL);
+    INIT_CLASS_ENTRY(ce, "Sleepy", sleepy_class_methods);
     ce.create_object = NULL;
     ce_Sleepy = zend_register_internal_class(&ce TSRMLS_CC);
 
@@ -33,6 +56,8 @@ PHP_MINIT_FUNCTION(sleepy)
 
     return SUCCESS;
 }
+
+//capi_request_context *rc = (capi_request_context *) SG(server_context);
 
 PHP_RINIT_FUNCTION(sleepy)
 {
