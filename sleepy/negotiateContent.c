ce0dcff203844868bdd75fc9bd3d638f1590f325
James Benner <james.benner@gmail.com>
2014-04-03 22:40:14 -0400

    Just a little slight tidying before I call it a night.

--- sleepy/negotiateContent.c
+++ sleepy/negotiateContent.c
@@ -6,29 +6,24 @@
 
 SLEEPY_METHOD(negotiateContent)
 {
-    zval *http_accept, *delim; /*, *accept_array;*/
-
-    /**
-     * This methodology is utilized in "main/php_variables.c" in the PHP source and it saved my sanity here.
-     */
-     zval *accept_array = NULL;
-
-    ALLOC_ZVAL(accept_array);
-    array_init(accept_array);
-    INIT_PZVAL(accept_array);
+    zval *http_accept, *delim;
 
     /*ZVAL_EMPTY_STRING(http_accept);*/
     MAKE_STD_ZVAL(http_accept);
     ZVAL_EMPTY_STRING(http_accept);
     MAKE_STD_ZVAL(delim);
-    /*array_init(accept_array);*/
-    CHECK_ZVAL_STRING(http_accept);
 
-/*
+
+    /**
+     * This methodology is utilized in "main/php_variables.c" in the PHP source
+     * and it saved my sanity here after quite a few hours...
+     */
+    zval *accept_array = NULL;
+
+    ALLOC_ZVAL(accept_array);
     array_init(accept_array);
-    array_init_size(accept_array, 10);
-    add_assoc_long(accept_array, "omg", 1337);
-*/
+    INIT_PZVAL(accept_array);
+
     /**
      * 3rd argv is "make a copy of string"
      */
@@ -54,10 +49,6 @@ SLEEPY_METHOD(negotiateContent)
      */
     php_explode(delim, http_accept, accept_array, 25);
 
-    /*
-    zval_ptr_dtor(delim);
-    zval_ptr_dtor(http_accept);
-    */
 
     /**
      * #include <zend_API.h>
@@ -73,12 +64,8 @@ SLEEPY_METHOD(negotiateContent)
      *
      * @see http://www.cs.unomaha.edu/local/php_manual/zend-macro.return-zval.html
      */
-    RETURN_ZVAL(accept_array, 0, 1);
+    RETURN_ZVAL(accept_array, 1, 1);
 
 
     /*php_var_dump(&accept_array, 1 TSRMLS_DC);*/
-
-    /*zval_dtor(&http_accept);*/
-
-    /*RETURN_ZVAL(&accept_array, 1, 1);*/
 }
